3820bed4fa7e27dc199d0cd04cf192470c8a4c6b
Saul Shanabrook <s.shanabrook@gmail.com>
2020-02-17 19:45:45 -0500

    New version

--- beni/__init__.py
+++ beni/__init__.py
@@ -16,7 +16,7 @@ import tqdm
 import typeguard
 import yaml
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 parser = argparse.ArgumentParser(description="Generate an environment.yml.")
 parser.add_argument(
